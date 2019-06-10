cf1089a793ccaca5e41acb2d8b92400505759b94
Alex Schmoe <aschmoe@gmail.com>
2019-06-10 13:18:58 -0700

    initial work to allow for typedev + callback information in the docs

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const dox = require('dox');
  *
  * @example formatStringForName('module.exports.parser');
  * @param {String} contents String to format.
+ * @param {String} [type=''] Type of method, eg: callback, typedef
  * @return {String} Formatted string.
  * @private
  */
 
-const formatStringForName = content =>
+const formatStringForName = (content, type = '') => {
+  let name = 
     content.toString().replace(/module\.exports\.|\.prototype|\(\)/gu, '');
+  
+  if (type == 'callback') {
+    name = `{Function} ${name}`;
+  }
+  
+  return name;
+};
 
 /**
  * Format string as param.
@@ -21,8 +30,36 @@ const formatStringForName = content =>
  * @private
  */
 
-const formatStringForParam = content =>
-    content.toString().replace(/\[|\]/gu, '');
+const formatStringForParam = content => 
+  content
+    .toString()
+    .replace(/\[|\]/gu, '');
+
+/**
+ * Format string with link to callbacks, typedefs.
+ *
+ * @example formatStringForParam('[optional param]');
+ * @param {String} contents String to format.
+ * @param {Object} linkableObjects List of on-page objects to link to
+ * @return {String} Formatted string.
+ * @private
+ */
+
+const formatStringWithLinkable = (content, linkableObjects = {}) => {
+  let string = content.toString().replace(/\[|\]/gu, '');
+  if (linkableObjects && linkableObjects[string]) {
+    // Use the link
+    string = `<a href="#${linkableObjects[string]}">${string}</a>`;
+  } else {
+    // Escape it
+    string = string.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
+      return '&#'+i.charCodeAt(0)+';';
+   });
+  }
+  return string;
+}
+
+
 
 /**
  * Format string as UID.
@@ -41,6 +78,15 @@ const formatStringForUID = content =>
         .replace(/^-|-$/gu, '');
 
 /**
+ * Checks if the current method is a callback or typedef
+ * 
+ * @param {Object} method The current method being parsed
+ * @return {Bool} If it is a callback or typedef
+ * @private
+ */
+const isCallbackOrTypedef = method => method.tags.length && (method.tags[0].type === 'typedef' || method.tags[0].type === 'callback')
+
+/**
  * Dox parser for doxdox.
  *
  * @example parser(content, 'index.js').then(methods => console.log(methods));
@@ -49,37 +95,49 @@ const formatStringForUID = content =>
  * @return {Promise} Promise with methods parsed from contents.
  * @public
  */
+const parser = (content, filename) => {
+    const linkableObjects = {};
 
-const parser = (content, filename) =>
-    dox
-        .parseComments(content, {
-            'raw': true,
-            'skipSingleStar': true
-        })
-        .filter(method => !method.ignore && method.ctx)
-        .map(method => ({
-            'uid': formatStringForUID(`${filename}-${method.ctx.string}`),
+    const methods = dox
+      .parseComments(content, {
+          'raw': true,
+          'skipSingleStar': true
+      })
+      .filter(method => !method.ignore);
+    
+    // First build linkable items
+    methods.forEach(method => {
+      if (isCallbackOrTypedef(method)) {
+        const name = formatStringForName(method.tags[0].string, method.tags[0].type)
+          .replace(/^\{[a-z]*\}\ /i, '');
+        const uid = formatStringForUID(`${filename}-${method.tags[0].string}`);
+        linkableObjects[name] = uid;
+      }
+    });
+    // console.log(linkableObjects);
+
+    // Build out
+    return methods.map(method => {
+          const params = {
             'isPrivate': method.isPrivate,
-            'type': method.ctx.type,
-            'name': formatStringForName(method.ctx.string),
             'description': method.description.full,
             'empty': !method.description.full && !method.tags.length,
             'params': method.tags
-                .filter(tag => tag.type === 'param' && !tag.name.match(/\./u))
-                .map(tag => {
+              .filter(tag => tag.type === 'param' && !tag.name.match(/\./u))
+              .map(tag => {
 
-                    if (tag.optional) {
+                  if (tag.optional) {
 
-                        return `[${formatStringForParam(tag.name)}]`;
+                      return `[${formatStringForParam(tag.name)}]`;
 
-                    }
+                  }
 
-                    return formatStringForParam(tag.name);
+                  return formatStringForParam(tag.name);
 
-                })
-                .join(', ')
-                .replace(/\], \[/gu, ', ')
-                .replace(', [', '[, '),
+              })
+              .join(', ')
+              .replace(/\], \[/gu, ', ')
+              .replace(', [', '[, '),
             'tags': {
                 'example': method.tags
                     .filter(tag => tag.type === 'example')
@@ -89,24 +147,63 @@ const parser = (content, filename) =>
                     .map(tag => ({
                         'name': formatStringForParam(tag.name),
                         'isOptional': tag.optional,
-                        'types': tag.types,
+                        'types': tag.types.map(type => formatStringWithLinkable(type, linkableObjects)),
                         'description': tag.description
                     })),
                 'property': method.tags
                     .filter(tag => tag.type === 'property')
                     .map(tag => ({
-                        'name': tag.name,
-                        'types': tag.types,
+                        'name': formatStringForParam(tag.name),
+                        'isOptional': tag.optional,
+                        'types': tag.types.map(type => formatStringWithLinkable(type, linkableObjects)),
                         'description': tag.description
                     })),
                 'return': method.tags
                     .filter(tag => tag.type === 'return' || tag.type === 'returns')
-                    .map(tag => ({
-                        'types': tag.types,
-                        'description': tag.description
-                    }))
+                    .map(tag => {
+                      // console.log(tag);
+                      return {
+                          'types': tag.types.map(type => formatStringWithLinkable(type, linkableObjects)),
+                          'description': tag.description
+                      }
+                    })
+              }
+          };
+          
+          if (method.ctx) {
+            // Normal method
+            params.uid = formatStringForUID(`${filename}-${method.ctx.string}`);
+            params.type = method.ctx.type;
+            params.name = formatStringForName(method.ctx.string);
+          } else if (isCallbackOrTypedef(method)) {
+            params.uid = formatStringForUID(`${filename}-${method.tags[0].string}`);
+            // console.log(params.uid);
+            params.type = method.tags[0].type;
+            params.name = formatStringForName(method.tags[0].string, params.type);
+            params.toBottom = true;
+            if (method.tags[0].type === 'typedef') {
+              params.skipReturns = true;
+              params.params = '';
             }
-        }))
-        .filter(method => !method.empty);
+
+          } else {
+            method.empty = true;
+          }
+
+          return params;
+
+        })
+        .filter(method => !method.empty)
+        .sort((a, b) => {
+          if (a.toBottom && b.toBottom) {
+            return a.type > b.type;
+          } else if (a.toBottom) {
+            return 1;
+          } else if (b.toBottom) {
+            return -1;
+          }
+          return 0;
+        })
+};
 
 module.exports = parser;
